817d388fe3061431be9c20698fe3ace5a44cbb35
Xinghan Cui <xinghan.cui@gmail.com>
2015-05-04 23:10:42 -0400

    Add User login page.

--- app/src/main/java/com/xinghan/android/loveandhelp/core/user/User.java
+++ app/src/main/java/com/xinghan/android/loveandhelp/core/user/User.java
@@ -8,14 +8,5 @@ import java.util.UUID;
  * Created by xinghan on 4/17/15.
  */
 public class User extends Role {
-
-    public String getPassword() {
-        return mPassword;
-    }
-
-    public void setPassword(String password) {
-        mPassword = password;
-    }
-
     private String mPassword;
 }
