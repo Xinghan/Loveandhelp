03bd4f682f153cefbc3a15f1936e391ea5808504
Xinghan Cui <xinghan.cui@gmail.com>
2015-05-10 21:49:17 -0400

    Add JSON utils

--- app/src/main/java/com/xinghan/android/loveandhelp/utils/JsonUtils.java
+++ app/src/main/java/com/xinghan/android/loveandhelp/utils/JsonUtils.java
@@ -1,8 +1,30 @@
 package com.xinghan.android.loveandhelp.utils;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Created by xinghan on 4/18/15.
  */
 public class JsonUtils {
+    private JSONArray mJSONArray;
+    private JSONObject mJSONObject;
+    private String mFilename;
+
+    JsonUtils(JSONArray jsonArray) {
+        this.mJSONArray = jsonArray;
+    }
+    JsonUtils(JSONObject jsonObject) {
+        this.mJSONObject = jsonObject;
+    }
+
+    public void setFilename (String filename) {
+        this.mFilename = filename;
+    }
+
+    public void saveJsonToFile() {
+
+
+    }
 
 }
